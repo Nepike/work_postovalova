083a0d8849030a16ba811638f89e980eba6041af
Максим Щучкин <maxventilator@gmail.com>
2025-10-10 01:30:14 +0300

    test

--- postovalova/urls.py
+++ postovalova/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('telegram_bot/', include('telegram_bot.urls')),
     path('', include('core.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
